9a00eff92c9c6e255bd36247c4d81190d5430184
MrCatDog <failoff.catdog@yandex.ru>
2022-03-22 01:23:52 +0300

    Handmade singleton excluded from UserDatabase.

--- app/src/main/java/com/example/users/utils/cachedatabase/UserDatabase.kt
+++ app/src/main/java/com/example/users/utils/cachedatabase/UserDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.users.utils.cachedatabase
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.users.mainfragment.FullUserInfo
@@ -10,23 +8,5 @@ import com.example.users.mainfragment.FullUserInfo
 @Database(entities = [FullUserInfo::class], version = 1, exportSchema = false)
 @TypeConverters(DataConverters::class)
 abstract class UserDatabase : RoomDatabase() {
-
     abstract fun userDao(): UserDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: UserDatabase? = null
-
-        fun getDatabase(context: Context): UserDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    UserDatabase::class.java,
-                    "user_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/users/utils/network/DataReciver.kt
+++ app/src/main/java/com/example/users/utils/network/DataReciver.kt
@@ -4,15 +4,13 @@ import retrofit2.*
 import kotlin.reflect.KFunction1
 import kotlin.reflect.KFunction2
 
-class DataReceiver(private val serverApi : ServerApi) {
+class DataReceiver(private val serverApi: ServerApi) {
 
     fun requestUsers(
         onResponse: KFunction1<Response<List<UserResponse>>, Unit>,
         onFail: KFunction2<Call<List<UserResponse>>, Throwable, Unit>
     ) {
-        val users = serverApi.getHotelList()
-
-        users.enqueue(object : Callback<List<UserResponse>> {
+        serverApi.getHotelList().enqueue(object : Callback<List<UserResponse>> {
 
             override fun onFailure(call: Call<List<UserResponse>>, t: Throwable) {
                 onFail(call, t)
