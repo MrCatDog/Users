1430de7ba28ade74f812c01ce5e32f4efd601b7b
MrCatDog <failoff.catdog@yandex.ru>
2022-03-22 21:42:08 +0300

    viewModelExt.kt excluded. DataConverters.kt slightly changed, gson is class property now.

--- app/src/main/java/com/example/users/utils/cachedatabase/DataConverters.kt
+++ app/src/main/java/com/example/users/utils/cachedatabase/DataConverters.kt
@@ -7,30 +7,28 @@ import com.google.gson.reflect.TypeToken
 
 class DataConverters {
 
+    private val gson = Gson()
+
     @TypeConverter
     fun fromBaseUserInfo(value: BaseUserInfo): String {
-        val gson = Gson()
         val type = object : TypeToken<BaseUserInfo>() {}.type
         return gson.toJson(value, type)
     }
 
     @TypeConverter
     fun toBaseUserInfo(value: String): BaseUserInfo {
-        val gson = Gson()
         val type = object : TypeToken<BaseUserInfo>() {}.type
         return gson.fromJson(value, type)
     }
 
     @TypeConverter
     fun fromFriendsIdSet(value: Set<Int>): String {
-        val gson = Gson()
         val type = object : TypeToken<Set<Int>>() {}.type
         return gson.toJson(value, type)
     }
 
     @TypeConverter
     fun toFriendsIdSet(value: String): Set<Int> {
-        val gson = Gson()
         val type = object : TypeToken<Set<Int>>() {}.type
         return gson.fromJson(value, type)
     }
